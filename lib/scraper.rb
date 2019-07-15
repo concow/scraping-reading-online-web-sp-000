3f39e952641241d33abc2f20984651c11671a401
Connor Cowan <ccowan003@comcast.net>
2019-07-15 21:11:24 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,6 +4,5 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(html)
- 
-doc.css(".tout__label.heading.heading--level-4")
 
+doc.css(".tout__label.heading.heading--level-4")
